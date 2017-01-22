1b81b03a1eaa9b4e29c6f3c67e16a1ae3fd6e3d6
Gabriel Esteban <gabriel.esteban.gullon@gmail.com>
2017-01-22 06:47:30 -0500

    Retoque al easter egg pal humorous hack

--- web/assets/js/webqr.js
+++ web/assets/js/webqr.js
@@ -151,7 +151,7 @@ function setwebcam2(options) {
 }
 
 function processAddress(address){
-    if("L-3jobs.com" in address){
+    if(adress.indexOf("L-3jobs.com") != -1){
         $('#results-content').append('<section id="intro" class="container"> \
             <div id="logo-container"></div>\
         </section> \
