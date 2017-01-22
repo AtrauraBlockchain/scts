c1cfe4e28e69d3fb3fbebf725f232940196cde37
Gabriel Esteban <gabriel.esteban.gullon@gmail.com>
2017-01-22 07:08:14 -0500

    Updated easter egg

--- web/assets/js/webqr.js
+++ web/assets/js/webqr.js
@@ -151,7 +151,7 @@ function setwebcam2(options) {
 }
 
 function processAddress(address){
-    if(address.indexOf("L-3jobs.com") != -1){
+    if(address.indexOf("-3jobs.com") != -1){
         $('#results-content').append('<section id="intro" class="container"> \
             <div id="logo-container"></div>\
         </section> \
